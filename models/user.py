518efa8cf1478f91c7a82416c8b7e5dafb54a0f4
Enoch <henokwon@gmail.com>
2022-12-13 15:17:00 -0500

    user updated

--- models/user.py
+++ models/user.py
@@ -9,9 +9,10 @@ from models.place import Place
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "User"
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-   
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
